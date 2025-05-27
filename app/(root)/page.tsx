fe8e93682efb89644d15645ecc6b5066826ddcad
nasii05 <iamnasih@gmail.com>
2025-05-27 16:52:34 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "@/components/Header";
+
+const Page = () => {
+  return (
+    <main className="wrapper page">
+        <Header title="All Videos" subHeader="Public Library" />
+        <h1 className="text-2xl font-karla">welcome to loom clone</h1>
+    </main>
+  )
+}
+
+export default Page
---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+
+const user = {};
+
+const Navbar = () => {
+
+
+    return (
+       <header className="navbar">
+           <nav>
+               <Link href="/">
+                   <Image src="/assets/icons/logo.svg" alt="logo"
+                   width={32} height={32}/>
+                   <h1>SnapCast</h1>
+               </Link>
+
+               {user && (
+                   <figure>
+                       <button>
+                           <Image src="/assets/images/dummy.jpg"
+                                  alt="user" width={36} height={36} className="rounded-full aspect-square"/>
+                       </button>
+
+                       <button className="cursor-pointer">
+                           <Image src="/assets/icons/logout.svg"
+                           alt="logout" width={24} height={24}
+                                  className="rotate-180 bg"/>
+                       </button>
+                   </figure>
+
+               )}
+           </nav>
+       </header>
+    )
+}
+export default Navbar
